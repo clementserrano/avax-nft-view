59295357bb01915f57f345a679778925a247daa8
clementserrano <clementserrano3@gmail.com>
2021-07-01 01:24:49 +0200

    🎉 Connect to avax wallet and display transactions img url
    The latest addresses are not fetched in the wallet, which leads to missing transactions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Avax wallet viewer for NFT
+
+Shows your NFT on a screen.
+
+Intended to use with a Raspberry with touch-screen.
+
+Not finished.
---
+++ main.ts
@@ -0,0 +1,20 @@
+import { MnemonicWallet } from '@avalabs/avalanche-wallet-sdk';
+import secrets from './secrets.json';
+
+const wallet = MnemonicWallet.fromMnemonic(secrets.mnemonic);
+
+// Log nfts img url from transactions history
+wallet.getHistory().then(histories => {
+    histories.forEach((history: any) => {
+        for (const address in history.nfts.received) {
+            for (const group in history.nfts.received[address].groups) {
+                const payload = JSON.parse(history.nfts.received[address].groups[group].payload);
+                console.log(payload.avalanche.img);
+            }
+        }
+    });
+})
+
+// Not every address is fetched
+console.log(wallet.getAllAddressesX());
+
